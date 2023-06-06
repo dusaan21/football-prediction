6457cf69606b35fe83ad72e49a3fc0b466baf73c
dusaan21 <dusan.a.stojanovic@gmail.com>
2023-06-07 01:45:06 +0200

    Network optimized

--- football-prediction.py
+++ football-prediction.py
@@ -8,6 +8,9 @@ Created on Sat Jun  3 22:46:34 2023
 import pandas as pd
 from tensorflow.keras import layers
 from tensorflow.keras import models
+from tensorflow.keras import regularizers
+from tensorflow.keras.callbacks import EarlyStopping
+
 import numpy as np
 
 #Ucitavamo podatke
@@ -41,8 +44,8 @@ training_data = dataset[dataset['Date'] < '08/01/2021']
 test_data = dataset[dataset['Date'] > '08/01/2021']
 
 #Delimo ulazne kolone na dva dela, jer cemo koristiti Sijamsku NM, gde ce nam jedna podmreza biti za domacu ekipu, a druga za gostujucu
-home_columns=['Hour','Day_Code', 'HomeTeam', 'HTHG', 'HS', 'HST', 'HC', 'HY', 'HR']
-away_columns=['Hour','Day_Code', 'AwayTeam', 'HTAG', 'AS', 'AST', 'AC', 'AY', 'AR']
+home_columns=['Hour','Day_Code', 'HomeTeam', 'HTHG', 'HS', 'HST', 'HC']
+away_columns=['Hour','Day_Code', 'AwayTeam', 'HTAG', 'AS', 'AST', 'AC']
 training_input_home = training_data[home_columns]
 training_input_away = training_data[away_columns]
 training_output_home = training_data['FTHG']
@@ -52,31 +55,44 @@ training_output_away = training_data['FTAG']
 input_shape = (len(home_columns),)
 hidden_units = 64
 
+l2_strength = 0.015
 #Definisemo podmrezu sa jednim slojem za domaci tim
 input_home = layers.Input(shape=input_shape)
-hidden_home1 = layers.Dense(hidden_units, activation='relu')(input_home)
-hidden_home2 = layers.Dense(hidden_units, activation='relu')(hidden_home1)
+hidden_home1 = layers.Dense(hidden_units, activation='LeakyReLU', kernel_regularizer=regularizers.l2(l2_strength))(input_home)
+hidden_home2 = layers.Dense(hidden_units/2, activation='LeakyReLU', kernel_regularizer=regularizers.l2(l2_strength))(hidden_home1)
+hidden_home3 = layers.Dense(hidden_units/4, activation='LeakyReLU', kernel_regularizer=regularizers.l2(l2_strength))(hidden_home2)
+hidden_home4 = layers.Dense(hidden_units/8, activation='LeakyReLU', kernel_regularizer=regularizers.l2(l2_strength))(hidden_home3)
+hidden_home5 = layers.Dense(1, activation='linear', kernel_regularizer=regularizers.l2(l2_strength))(hidden_home4)
+
+
 
 #Definisemo podmrezu sa jednim slojem za gostujuci tim
 input_away = layers.Input(shape=input_shape)
-hidden_away1 = layers.Dense(hidden_units, activation='relu')(input_away)
-hidden_away2 = layers.Dense(hidden_units, activation='relu')(hidden_away1)
+hidden_away1 = layers.Dense(hidden_units, activation='LeakyReLU', kernel_regularizer=regularizers.l2(l2_strength))(input_away)
+hidden_away2 = layers.Dense(hidden_units/2, activation='LeakyReLU', kernel_regularizer=regularizers.l2(l2_strength))(hidden_away1)
+hidden_away3 = layers.Dense(hidden_units/4, activation='LeakyReLU', kernel_regularizer=regularizers.l2(l2_strength))(hidden_away2)
+hidden_away4 = layers.Dense(hidden_units/8, activation='LeakyReLU', kernel_regularizer=regularizers.l2(l2_strength))(hidden_away3)
+hidden_away5 = layers.Dense(1, activation='linear', kernel_regularizer=regularizers.l2(l2_strength))(hidden_away4)
+
+
 
 #Koristimo sloj Concatenate da bi spojili izlaze iz prethodne dve podmreze
-concatenated = layers.Concatenate()([hidden_home1, hidden_away2])
+concatenated = layers.Concatenate()([hidden_home5, hidden_away5])
 
 #Kreiramo dva izlazna sloja
 output_home = layers.Dense(1, activation='linear', name='output_home')(concatenated)
 output_away = layers.Dense(1, activation='linear', name='output_away')(concatenated)
 
+
 #Kreiramo model za Sijamsku NM
 sm = models.Model(inputs=[input_home, input_away], outputs=[output_home, output_away])
 
 
-sm.compile(optimizer='adam', loss='mse')
+sm.compile(optimizer='adam', loss='mae')
 
+early_stop = EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=True)
 
-sm.fit([training_input_home, training_input_away], [training_output_home, training_output_away], epochs=20, batch_size=32, validation_split=0.2)
+sm.fit([training_input_home, training_input_away], [training_output_home, training_output_away], epochs=500, batch_size=32, validation_split=0.2, callbacks=[early_stop])
 
 
 test_input_home = test_data[home_columns]
@@ -88,14 +104,22 @@ test_output_away = test_data['FTAG'].values.ravel()
 
 predicted_home_goals, predicted_away_goals = sm.predict([test_input_home, test_input_away])
 
-predicted_home_goals = predicted_home_goals.astype(int)
-predicted_away_goals = predicted_away_goals.astype(int)
+#Zaokruzujemo na ceo broj nase predikcije
+predicted_home_goals = predicted_home_goals.round().astype(int)
+predicted_away_goals = predicted_away_goals.round().astype(int)
+
+#Negativne vrednosti stavljamo na nulu
+predicted_home_goals = np.maximum(predicted_home_goals, 0)
+predicted_away_goals = np.maximum(predicted_away_goals, 0)
 
 combined = pd.DataFrame(dict(actual=test_output_home, prediction=predicted_home_goals.flatten()))
+print("Crosstab for Home Team: ")
 print(pd.crosstab(index = combined["actual"], columns=combined["prediction"]))
-
-
 accuracy = (predicted_home_goals.flatten() == test_output_home).mean()
-
 print("Precision Accuracy for Home Team:", accuracy)
 
+combined = pd.DataFrame(dict(actual=test_output_away, prediction=predicted_away_goals.flatten()))
+print("Crosstab for Away Team: ")
+print(pd.crosstab(index = combined["actual"], columns=combined["prediction"]))
+accuracy = (predicted_away_goals.flatten() == test_output_away).mean()
+print("Precision Accuracy for Home Team:", accuracy)
